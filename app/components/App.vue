4645888ff3195f3ce889e574082624bba425e998
ll9 <llwierer@web.de>
2018-11-13 21:20:21 +0100

    basic tiles on startup

---
+++ app/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*, *" rows="200, 200">
+            <Button text="Karte" col="0" row="0" />
+            <Button text="Erfassen" col="1" row="0" />
+            <Button text="Bestand" col="0" row="1" />
+            <Button text="Einstellungen" col="1" row="1" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
